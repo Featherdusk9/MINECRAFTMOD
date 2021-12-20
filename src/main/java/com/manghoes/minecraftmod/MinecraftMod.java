e69ae1544c79b274449528186a9207df8078d100
Featherdusk9 <90982699+Featherdusk9@users.noreply.github.com>
2021-12-19 23:41:40 -0500

    set up

---
+++ src/main/java/com/manghoes/minecraftmod/MinecraftMod.java
@@ -0,0 +1,13 @@
+package com.manghoes.minecraftmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = MinecraftMod.MODID)
+public class MinecraftMod {
+	public static final String MODID = "minecraftmod";
+	
+	public MinecraftMod() {
+		
+	}
+
+}
